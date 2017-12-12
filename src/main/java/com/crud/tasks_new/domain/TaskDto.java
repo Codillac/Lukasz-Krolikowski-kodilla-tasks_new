a38c214eeb932b3108e318de4b4681d574d90881
“Codillac <luk.krolikowski@gmail.com>
2017-12-12 20:18:05 +0100

    fixes

--- src/main/java/com/crud/tasks_new/domain/TaskDto.java
+++ src/main/java/com/crud/tasks_new/domain/TaskDto.java
@@ -1,11 +1,13 @@
 package com.crud.tasks_new.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TaskDto {
 
     @JsonProperty("id")
